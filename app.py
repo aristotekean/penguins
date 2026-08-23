5c23c1611dce50644e36d0756e98e2e2f7651567
aristotekean <kean.solano@gmail.com>
2026-08-23 14:07:55 -0500

    fix: import HTTPException in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from typing import Literal
 
 import joblib
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 
 MODEL_PATH = Path(__file__).parent
